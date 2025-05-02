43d28bfa4a8222a9be5fc73221edd39f3663f2e1
Joylynn Kirui <joylynnc.kirui@gmail.com>
2025-05-02 14:47:39 +0300

    Lab350

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "Microsoft Build Lab"
+include(":app")
+ 
